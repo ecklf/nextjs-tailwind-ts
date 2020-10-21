08174a053451427bd80ba763b5e0d3acd2862d47
impulse <ecklf@icloud.com>
2020-10-21 10:48:05 +0200

    build: use tailwind v2 alpha

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: [],
+  darkMode: "media",
   theme: {
     extend: {},
   },
