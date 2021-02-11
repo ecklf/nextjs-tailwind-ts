2c4625e0eb0c8b7c0088bce448eebdf500e82b5b
impulse <ecklf@icloud.com>
2021-02-11 20:00:00 +0100

    build: update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@ module.exports = {
   purge: [],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {
-      padding: { "fluid-video": "56.25%" },
-    },
+    extend: {},
   },
   variants: {},
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
